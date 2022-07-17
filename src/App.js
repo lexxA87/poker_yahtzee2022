82e206340e816b1589282e2ca8e95951e2fd401b
lexxA87 <alex.o.nikonov@gmail.com>
2022-07-17 20:38:02 +0400

    Added menu component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Navigate, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Game from "./components/Game";
+import Menu from "./components/menu/Menu";
 import TestAnimation from "./components/testAnimation/TestAnimation";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route index path="/" element={<Game />} />
           <Route index path="/test" element={<TestAnimation />} />
+          <Route index path="/menu" element={<Menu />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+function Menu() {
+  return (
+    <div
+      className="d-grid gap-4 
+    col-12 col-sm-8 col-md-6 
+    col-lg-3 mx-auto position-absolute 
+    top-50 start-50 translate-middle"
+    >
+      <Button>Play</Button>
+      <Button>Statistic</Button>
+      <Button>Reiting</Button>
+      <Button>Login/Logout</Button>
+    </div>
+  );
+}
+
+export default Menu;
